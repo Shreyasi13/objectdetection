d7d446d3b737beecadf315464452fbe79cc0e06b
Shreyasi13 <76837372+Shreyasi13@users.noreply.github.com>
2022-03-22 10:37:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function catsdog(){
+    window.location = "cat_dogs.html";
+}
+function bedroom(){
+    window.location = "bedroom.html";
+}
+function pizza2(){
+    window.location = "piz_za.html";
+}
